2e39aa63caed631f9dd614ca7f4f3083bc0d9a43
sgard62@yahoo.com <anthonysgard@MacBook-Air.local>
2021-11-24 16:55:50 +0100

    Deployed to Rinkeby

--- deploy.js
+++ deploy.js
@@ -12,6 +12,13 @@ const web3 = new Web3(provider);
 const deploy = async () =>{
     const accounts = await web3.eth.getAccounts();
     console.log(accounts);
+    console.log('Contract is deployed by the Mgr with adress ', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data : '0x'+bytecode})
+    .send({gas : '2000000', from : accounts[0]})
+
+    console.log('Contract deployed to address', result.options.address);
 
 }
 
--- lottery.js
+++ lottery.js
@@ -1,5 +1,5 @@
 import web3 from './web3.js';
-const address ='';
+const address ='0xF759e35cf53Aa6CEb1e5973A7e1Fdf9A304DFCb8';
 const abi =
 [{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"participants","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"enterLottery","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
 
