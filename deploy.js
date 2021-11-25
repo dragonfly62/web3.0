71c75d6bb682b69e8aea57592eda8076908cfc78
sgard62@yahoo.com <anthonysgard@MacBook-Air.local>
2021-11-25 10:36:37 +0100

    getting Web3 installed

--- deploy.js
+++ deploy.js
@@ -12,14 +12,14 @@ const web3 = new Web3(provider);
 const deploy = async () =>{
     const accounts = await web3.eth.getAccounts();
     console.log(accounts);
-    console.log('Contract is deployed by the Mgr with adress ', accounts[0]);
+    console.log('Contract is deployed by the Mgr with address ', accounts[0]);
 
     const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({data : '0x'+bytecode})
     .send({gas : '2000000', from : accounts[0]})
 
     console.log('Contract deployed to address', result.options.address);
-
+//0x95f2f1A9588ccA74d60b35619ce25bba754bfB77//
 }
 
 deploy();
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,35 @@
 import React, {Component} from 'react';
 
+import lottery from '../lottery';
+import web3 from '../web3';
+
 class Lottery extends Component{
+
+    constructor(props){
+        super(props);
+        this.state ={
+            manager :'',
+            participate_amount : '0.5',
+            message : ''
+
+        }
+    }
+    async componentDidMount(){
+        //get public address of manager//
+        const manager = await lottery.methods.manager().call();
+        console.log(manager);
+        this.setState({manager : manager });
+    }
     render(){
         return (
             <div>
                 <h1>Total lottery pool is 1000 </h1>
                 <form> 
-                    <input value= "0.5" />
-                    <button type="submit">PArticipate </button>
+                    <input placeholder= "0.5" />
+                    <button type="submit">Participate </button>
                 </form>
                 <hr /> <br /> <hr />
-                <p>The manager of the lottery decentralized app is </p>
+                <p>The manager of the lottery decentralized app is {this.state.manager} </p>
                 <button> Pick Winner </button>
             </div>
             
